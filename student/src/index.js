37cb997c3492f1b0fac730be1fec110419bac155
Stephen Caldwell <steve@campusexplorer.com>
2017-01-19 14:11:14 -0800

    setup hot reloading of components

--- student/src/index.js
+++ student/src/index.js
@@ -2,15 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './App/configureStore';
-import App from './App';
 
 import "semantic-ui-css/semantic.css";
 
 const store = configureStore();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+const rootEl = document.getElementById("root");
+
+let render = () => {
+    const App = require("./App").default;
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootEl
+    );
+};
+
+if(module.hot) {
+    const renderApp = render;
+    const renderError = (error) => {
+        const RedBox = require("redbox-react").default;
+        ReactDOM.render(
+            <RedBox error={error} />,
+            rootEl,
+        );
+    };
+
+    render = () => {
+        try {
+            renderApp();
+        }
+        catch(error) {
+            console.error(error);
+            renderError(error);
+        }
+    };
+    module.hot.accept("./App", () => {
+        setTimeout(render);
+    });
+}
+
+render();
