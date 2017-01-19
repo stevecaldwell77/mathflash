55d0f097ab0ec32b8b23ca0efee99b5af29230ad
Stephen Caldwell <steve@campusexplorer.com>
2017-01-19 11:42:42 -0800

    choose scene based on state

--- student/src/App/index.js
+++ student/src/App/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import '../App.css';
-
+import { getActiveCircuit, getCurrentStudent } from './selectors';
 import Login from '../scenes/Login';
-// import Start from '../scenes/Start';
-// import Circuit from '../scenes/Circuit';
+import Start from '../scenes/Start';
+import Circuit from '../scenes/Circuit';
+
+const chooseScene = state =>
+    getActiveCircuit(state)    ? Circuit
+    : getCurrentStudent(state) ? Start
+                               : Login;
 
 export const mapState = state => ({
-    Scene: Login,
+    Scene: chooseScene(state),
 });
 
 export const Component = ({ Scene }) => <Scene />
--- student/src/App/index.test.js
+++ student/src/App/index.test.js
@@ -1,9 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Component } from './';
+import { fromJS } from 'immutable';
+import { mapState, Component } from './';
+import Login from '../scenes/Login';
+import Start from '../scenes/Start';
+import Circuit from '../scenes/Circuit';
 
 it('component without crashing', () => {
-  const div = document.createElement('div');
-  const scene = () => <div>hello world</div>;
-  ReactDOM.render(<Component Scene={scene} />, div);
+    const div = document.createElement('div');
+    const scene = () => <div>hello world</div>;
+    ReactDOM.render(<Component Scene={scene} />, div);
+});
+
+test('mapState: choose circuit scene', () => {
+    const state = fromJS({
+        entities: {
+            students: {
+                '1': {
+                    studentName: 'John Doe',
+                },
+            },
+            circuits: {
+                '1': {
+                    startTime: 10,
+                }
+            },
+        },
+        session: {
+            currentStudentId: '1',
+        }
+    });
+    const result = mapState(state);
+    expect(result).toEqual({
+        Scene: Circuit,
+    });
+});
+
+test('mapState: choose start scene', () => {
+    const state = fromJS({
+        entities: {
+            students: {
+                '1': {
+                    studentName: 'John Doe',
+                },
+            },
+            circuits: {
+                '1': {
+                    startTime: 10,
+                    endTime: 20,
+                }
+            },
+        },
+        session: {
+            currentStudentId: '1',
+        }
+    });
+    const result = mapState(state);
+    expect(result).toEqual({
+        Scene: Start,
+    });
+});
+
+test('mapState: choose login scene', () => {
+    const state = fromJS({
+        entities: {
+            students: {
+                '1': {
+                    studentName: 'John Doe',
+                },
+            },
+            circuits: {
+            },
+        },
+    });
+    const result = mapState(state);
+    expect(result).toEqual({
+        Scene: Login,
+    });
 });
